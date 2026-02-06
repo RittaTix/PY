b96b09b33db4569e4f928b5da5c50bc41416a6a1
RittaTix <tikhobaeva.m@yandex.ru>
2026-02-06 17:35:23 +0300

    stepik
    stepik

---
+++ STEPIK/02/121.py
@@ -0,0 +1,34 @@
+# import random
+
+# n = int(input())    # количество попыток
+# monetka = {1:"Решка", 2:"Орёл"}
+# for i in range(n):
+#     num = random.randint(1,2)
+#     print(monetka[num])
+
+# from random import randint
+
+# COIN = ['Орел', 'Решка']
+
+# for _ in range(int(input())):
+#     print(COIN[randint(0, 1)])
+
+
+# from random import randint
+# for _ in range(int(input())):
+#      print(randint(1, 6))
+
+# chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+ 
+
+# chars = 'abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+# from random import randint
+# for _ in range(int(input())):
+#     print(chars[randint(1, len(chars))], end="")
+
+# from random import randint
+# dict = set()
+# while len(dict) < 7:
+#     dict.add(randint(1,49))
+# print(*sorted(dict))
